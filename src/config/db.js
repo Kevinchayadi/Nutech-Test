a56fd363a614cade7c2b1d8ae8218cafa9ef9dfe
Kevinchayadi <145884788+Kevinchayadi@users.noreply.github.com>
2025-05-19 19:59:01 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,19 @@ import { Pool } from 'pg';
 import config from '../config/index.js';
 
 const pool = new Pool({
-    host: config().db.host,
-    port: config().db.port,
-    user: config().db.user,
-    password: config().db.password,
-    database: config().db.name,
-  });
+  connectionString: postgresql://postgres:hudBOjBzbwywfNjcdSiDIepTOuDHbLUx@postgres.railway.internal:5432/railway,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
+});
+
 export default pool;
+
+// const pool = new Pool({
+//     host: config().db.host,
+//     port: config().db.port,
+//     user: config().db.user,
+//     password: config().db.password,
+//     database: config().db.name,
+//   });
+// export default pool;
